4405c7df6104250d71ee8731d00391db1b00e30e
Iain Earl <iain@itmecho.com>
2021-12-09 12:19:17 +0000

    Day 3 part 2 in rust

--- 2021/day-3/src/bin/part_2.rs
+++ 2021/day-3/src/bin/part_2.rs
@@ -1,3 +1,88 @@
 fn main() {
-    day_3::part_2();
+    let input = day_3::get_input();
+    let threshold = input.len() / 2;
+    let mut counts = [0; 16];
+
+    for value in &input {
+        for idx in 0..16 {
+            if value & (1 << (15 - idx)) > 0 {
+                counts[idx] = counts[idx] + 1
+            }
+        }
+    }
+
+    let mut gamma_rate: u16 = 0;
+    let mut epsilon_rate: u16 = 0;
+
+    let mut oxygen = input.clone();
+    let mut co2 = input.clone();
+
+    for idx in 0..16 {
+        let bitmask = 1 << (15 - idx);
+        if counts[idx] == 0 {
+            continue;
+        }
+
+        if counts[idx] >= threshold {
+            gamma_rate = gamma_rate | bitmask;
+        } else {
+            epsilon_rate = epsilon_rate | bitmask;
+        }
+
+        if oxygen.len() > 1 {
+            oxygen = filter(oxygen, bitmask, |value| value & bitmask > 0)
+        }
+
+        if co2.len() > 1 {
+            co2 = filter(co2, bitmask, |value| value & bitmask == 0)
+        }
+    }
+
+    if oxygen.len() != 1 {
+        panic!("found multiple oxygen measurements");
+    }
+
+    let oxygen_rate = oxygen.first().unwrap();
+
+    if co2.len() != 1 {
+        panic!("found multiple oxygen measurements");
+    }
+
+    let co2_rate = co2.first().unwrap();
+
+    println!("Gamma Rate: {}", gamma_rate);
+    println!("Epsilon Rate: {}", epsilon_rate);
+    println!("Answer #1: {}", gamma_rate as u64 * epsilon_rate as u64);
+
+    println!("\nOxygen Generator Rating: {}", oxygen_rate);
+    println!("CO2 Scrubber rating: {}", co2_rate);
+    println!("Answer #2: {}", *oxygen_rate as u64 * *co2_rate as u64);
+}
+
+fn get_count(input: &Vec<u16>, bitmask: u16) -> usize {
+    let mut count = 0;
+    for value in input {
+        if value & bitmask > 0 {
+            count = count + 1;
+        }
+    }
+    count
+}
+
+fn filter<F: FnMut(&u16) -> bool>(input: Vec<u16>, bitmask: u16, mut condition: F) -> Vec<u16> {
+    let ones = get_count(&input, bitmask);
+    if ones == 0 {
+        return input;
+    }
+
+    let zeros = input.len() - ones;
+
+    if ones >= zeros {
+        input.into_iter().filter(condition).collect()
+    } else {
+        input
+            .into_iter()
+            .filter(|value| !condition(value))
+            .collect()
+    }
 }
