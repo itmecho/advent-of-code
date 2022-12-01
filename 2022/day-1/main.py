72f1e31c7c1e38d696481865aa9ca2fc01cbd659
Iain Earl <iain@itmecho.com>
2022-12-01 15:02:53 +0000

    Tidy up 2022 day 1

--- 2022/day-1/main.py
+++ 2022/day-1/main.py
@@ -1,44 +1,14 @@
 from itertools import groupby
-import time
 import sys
 
-def timing(f):
-    def wrap(*args, **kwargs):
-        time1 = time.time()
-        ret = f(*args, **kwargs)
-        time2 = time.time()
-        print('{:s} function took {:.3f} ms'.format(f.__name__, (time2-time1)*1000.0))
+with open(sys.argv[1], 'r') as f:
+    lines = [line.strip() for line in f.readlines()]
+    elves = [sum(map(int, elf))
+             for value, elf in groupby(lines, key=bool) if value]
 
-        return ret
-    return wrap
-
-@timing
-def gross(f):
-    elves = [sum(map(int, list(elf))) for val, elf in groupby([line.strip() for line in f.readlines()], key = bool) if val]
-    elves.sort(reverse=True)
-    print(f"part 1: {elves[0]}")
-    print(f"part 2: {sum(elves[0:3])}")
-
-@timing
-def better(f):
-    elves = [0]
-    lines = f.readlines()
-    elf = 0
-    for line in lines:
-        line = line.strip()
-        if line == '':
-            elf += 1
-            elves.append(0)
-        else:
-            elves[elf] += int(line)
+    answer = max(elves)
+    print(f"part 1: {answer}")
 
     elves.sort(reverse=True)
-
-    print(f"part 1: {elves[0]}")
-    print(f"part 2: {sum(elves[0:3])}")
-
-with open(sys.argv[1]) as f:
-    better(f)
-
-with open(sys.argv[1]) as f:
-    gross(f)
+    answer = sum(elves[0:3])
+    print(f"part 2: {answer}")
